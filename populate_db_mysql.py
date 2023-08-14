ed8727119eea69e6c76798c36c63809eca086e94
Akrillai <Marks.Legkovskis@warwick.ac.uk>
2023-08-14 23:22:23 +0100

    First Commit

--- populate_db_mysql.py
+++ populate_db_mysql.py
@@ -2,14 +2,34 @@ import mysql.connector
 import numpy as np
 import time
 
+
 # Configuration parameters for MySQL
 config = {
     'user': 'root',
     'password': 'password',
     'host': 'db',
     'database': 'mydb',
+    'port': '3306',
     'raise_on_warnings': True
 }
+
+def create_table():
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    time.sleep(5)
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS my_table (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        x1 FLOAT NOT NULL,
+        x2 FLOAT NOT NULL,
+        x3 FLOAT NOT NULL
+    );
+    """
+
+    cursor.execute(create_table_query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
 
 def populate_db():
     cnx = mysql.connector.connect(**config)
@@ -25,6 +45,7 @@ def populate_db():
     cnx.close()
 
 if __name__ == "__main__":
+    create_table()
     while True:
         populate_db()
-        time.sleep(5)
+        time.sleep(2)
